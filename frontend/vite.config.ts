469718f514248c11a459178211e643dbf42c086f
shiroe66 <shiroeqky@gmail.com>
2023-01-07 22:09:25 +0500

    refactor: alias path

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -6,7 +6,7 @@ import path from 'path';
 export default defineConfig({
   resolve: {
     alias: {
-      '@app': path.resolve(__dirname, './src'),
+      '@': path.resolve(__dirname, './src'),
     },
   },
   plugins: [react()],
